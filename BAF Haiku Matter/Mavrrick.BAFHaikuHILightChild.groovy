5e14fff59894b17e25a48ef09cfcfc32af9269c7
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2024-11-05 14:08:03 -0500

    Update Mavrrick.BAFHaikuHILightChild.groovy

--- BAF Haiku Matter/Mavrrick.BAFHaikuHILightChild.groovy
+++ BAF Haiku Matter/Mavrrick.BAFHaikuHILightChild.groovy
@@ -36,6 +36,180 @@ metadata {
     }
 }
 
+//parsers
+void parse(Map descMap) {
+    if (logEnable) log.debug "Recieved Event from parent: descMap:${descMap}"   
+        switch (descMap.cluster) {
+            case "001D" :
+                if (descMap.attrId == "0001") { //list Clusters for endpoint
+                    state.endpointlist = [:]
+                    List clusters = []
+                    if (logEnable) log.debug "parse(): Cluster data found Endpoint:${descMap.endpoint}, Cluster returned${descMap.value}"
+                    descMap.value.forEach{
+                        count = it.length()
+                        if (count == 2) {
+                            clustCon = "00"+it
+                        } else if (count == 3) {
+                            clustCon = "0"+it
+                        } else {
+                        }
+                        clusters.add(clustCon)
+                    }
+                    state.endpointlist.put(descMap.endpoint,clusters)
+                }
+                break
+        case "0006" :
+            if (descMap.attrId == "0000") { //switch
+                sendSwitchEvent(descMap.value)
+            }
+            break
+        case "0000" :
+            if (descMap.attrId == "4000") { //software build
+                updateDataValue("softwareBuild",descMap.value ?: "unknown")
+            }
+            break
+        case '0008' :   // LevelControl
+            if (descMap.attrId == '0000') { //current level
+                sendLevelEvent(descMap.value)
+            }
+            else {
+                if (logEnable) log.debug "skipped level, attribute:${descMap.attrId}, value:${descMap.value}"
+            }
+//            gatherAttributesValuesInfo(descMap, LevelControlClusterAttributes)
+            break
+        case "0202" :
+            if (descMap.attrId == "0000") { //fan speed
+                sendFanModeEvent(descMap.value)
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed ${descMap.value}"
+            } else if (descMap.attrId == "0001") { //fan speed mode
+                if (logEnable) log.debug "parse(): Fan Event - Fan speed mode ${descMap.value}"
+            } else if (descMap.attrId == "0002") { //fan speed Percent Setting
+                sendSpeedEvent(descMap.value) 
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed  Percent ${descMap.value}"
+            } else  if (descMap.attrId == "0003") { //fan speed Percent current
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed Percent Curent ${descMap.value}"
+            } else if (descMap.attrId == "0004") { //fan speed max (Don't expect to actually ever return in parse
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed Max speed ${descMap.value}"
+            } else if (descMap.attrId == "0005") { //fan speed setting
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed setting ${descMap.value}"
+            } else if (descMap.attrId == "0006") { //fan speed setting current
+                if (logEnable) log.debug "parse(): Fan Event - Fan Speed current ${descMap.value}"
+            } else if (descMap.attrId == "000A") { //WindSetting current
+                sendWindMode(descMap.value)
+                if (logEnable) log.debug "parse(): Fan Event - Wind Speed setting ${descMap.value}"
+            } else  if (descMap.attrId == "000B") { //Airflow Direction
+                if (logEnable) log.debug "parse(): Fan Event - Airflow Direction setting ${descMap.value}"
+                sendFanDirEvent(descMap.value)
+            } else {
+                if (logEnable) log.debug "parse: skipped fan, attribute:${descMap.attrId}, value:${descMap.value}"
+            }
+//            gatherAttributesValuesInfo(descMap, FanClusterAttributes)
+            break
+        case '0300' :   // ColorControl
+            if (descMap.attrId == '0000') { //hue                
+                sendHueEvent(descMap.value)
+            } else if (descMap.attrId == '0001') { //saturation
+                sendSaturationEvent(descMap.value)
+            }
+            else if (descMap.attrId == '0007') { //color temperature
+                sendCTEvent(descMap.value)
+            }
+            else if (descMap.attrId == '0008') { //color mode
+                if (logEnable) log.debug "parse: skipped color mode:${descMap}"
+            }
+            else {
+                if (logEnable) log.debug "parse: skipped color, attribute:${descMap.attrId}, value:${descMap.value}"
+            }
+//            gatherAttributesValuesInfo(descMap, ColorControlClusterAttributes)
+            break
+        case '0402' :   // Temp Sensor
+            if (logEnable) log.debug "parse: Temp Sensor Information, attribute:${descMap.attrId}, value:${descMap.value}"
+            sendTempEvent(descMap.value)              
+            break  
+        case '0406' :   // Occupancy Sensor
+                if (logEnable) log.debug "parse: Occupancy sensor Information, attribute:${descMap.attrId}, value:${descMap.value}"
+            sendOccupancyEvent(descMap.value)
+            break 
+        default :
+            if (logEnable) {
+                log.debug "skipped:${descMap}"
+            }
+        
+    }
+}
+
+//events
+private void sendSwitchEvent(String rawValue) {
+    String value = rawValue == "01" ? "on" : "off"
+    if (device.currentValue("switch") == value) return
+    String descriptionText = "${device.displayName} was turned ${value}"
+    if (txtEnable) log.info descriptionText
+//    device = getChildDevice("${device.deviceNetworkId}-${CT_ENDPOINT}")
+    if (logEnable) log.debug "sendSwitchEvent(): Processing Switch event"
+    sendEvent(name:"switch", value:value, descriptionText:descriptionText)
+}
+
+private void sendLevelEvent(String rawValue) {
+    Integer value = Math.round(hexStrToUnsignedInt(rawValue) / 2.55)
+    if (value == 0 || value == device.currentValue("level")) return
+    String descriptionText = "${device.displayName} level was set to ${value}%"
+    if (txtEnable) log.info descriptionText
+//    device = getChildDevice("${device.deviceNetworkId}-${CT_ENDPOINT}")
+    if (logEnable) log.debug "sendLevelEvent(): Processing level event"
+    sendEvent(name:"level", value:value, descriptionText:descriptionText, unit: "%")
+}
+
+private void sendHueEvent(String rawValue, Boolean presetColor = false) {
+    Integer value = hex254ToInt100(rawValue)
+    if (device.currentValue("hue") != value ) {
+    sendRGBNameEvent(value)
+    String descriptionText = "${device.displayName} hue was set to ${value}%"
+    if (txtEnable) log.info descriptionText
+    sendEvent(name: "colorMode", value: "RGB")
+    sendEvent(name:"hue", value:value, descriptionText:descriptionText, unit: "%")
+    }   
+}
+
+private void sendSaturationEvent(String rawValue, Boolean presetColor = false) {
+    Integer value = hex254ToInt100(rawValue)
+    if (device.currentValue("saturation") != value ) {
+    sendRGBNameEvent(null,value)
+    String descriptionText = "${device.displayName} saturation was set to ${value}%"
+    if (txtEnable) log.info descriptionText
+    sendEvent(name: "colorMode", value: "RGB")
+    sendEvent(name:"saturation", value:value, descriptionText:descriptionText, unit: "%")
+    }
+}
+
+private void sendRGBNameEvent(hue, sat = null){
+    String genericName
+    if (device.currentValue("saturation") == 0) {
+        genericName = "White"
+    } else if (hue == null) {
+        return
+    } else {
+        genericName = colorRGBName.find{k , v -> hue < k}.value
+    }
+    if (genericName == device.currentValue("colorName")) return
+    String descriptionText = "${device.displayName} color is ${genericName}"
+    if (txtEnable) log.info descriptionText    
+    sendEvent(name: "colorName", value: genericName ,descriptionText: descriptionText)
+}
+
+private void sendCTEvent(String rawValue, Boolean presetColor = false) { 
+    if ((hexStrToUnsignedInt(rawValue)) != 0 ) {
+        value = (Math.round(10000/(hexStrToUnsignedInt(rawValue))))*100
+        if (value != device.currentValue("colorTemperature")) {
+            String descriptionText = "${device.displayName} ColorTemp was set to ${value}K" 
+        if (txtEnable) log.info descriptionText
+//        device = getChildDevice("${device.deviceNetworkId}-${CT_ENDPOINT}")
+        if (logEnable) log.debug "sendCTEvent(): Processing CT event"    
+        sendEvent(name:"colorTemperature", value:value, descriptionText:descriptionText, unit: "K")
+        sendEvent(name: "colorMode", value: "CT") 
+        }
+    }
+}
+
 
 //capability commands
 void on() { 
